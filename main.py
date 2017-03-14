56557f2e7b5f7eef8abb6133a742dddc85fce52a
xudashuai <xudashuai0819@outlook.com>
2017-03-15 00:12:50 +0800

    1.1.3

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ import os
 app = Flask(__name__)
 groups = {}
 bootstrap = Bootstrap(app)
-article_path=".\\static\\articles\\"
+article_path="./static/articles/"
 
 @app.route('/')
 def hello_world():
 	init()
-	with open('.\\第五次进展报告.md', 'r', encoding='utf-8') as f:
+	with open('./第五次进展报告.md', 'r', encoding='utf-8') as f:
 		return render_template('md.html', md=markdown(f.read(), output_format='html4',extensions=['markdown.extensions.extra', 'codehilite']), title='第五次进展报告', groups=groups)
 
 
 @app.route('/<group>/<article>')
 def show_article(group,article):
-	with open('{0}\\{1}\\{2}'.format(article_path,group,article), 'r', encoding='utf-8') as f:
+	with open('{0}/{1}/{2}'.format(article_path,group,article), 'r', encoding='utf-8') as f:
 		return render_template('md.html', md=markdown(f.read(), output_format='html4', extensions=['markdown.extensions.extra', 'codehilite']), title='第五次进展报告', groups=groups)
 
 
@@ -25,7 +25,7 @@ def init():
 	folders =	os.listdir(article_path)
 	for folder in folders:
 		groups[folder]={}
-		for article in os.listdir(article_path+'\\'+folder):
+		for article in os.listdir(article_path+'/'+folder):
 			groups[folder][article]='{0}/{1}'.format(folder,article)
 	print(groups)
 
